e1df6120ef30ce28a118017d9b9f27c16d36a61c
Alex Sologoub <alexsologoub@gmail.com>
2014-05-10 20:05:54 +0300

    Grinder: Basic tests

--- spec/lib/chef/knife/grinder_spec.rb
+++ spec/lib/chef/knife/grinder_spec.rb
@@ -1,4 +1,19 @@
 require 'spec_helper'
 
 describe Chef::Knife::Grinder do
+  context 'ancestry' do
+    it 'should include Knife base class' do
+      described_class.should < Chef::Knife
+    end
+  end
+
+  context '#run' do
+    it 'should be defined' do
+      subject.should respond_to(:run)
+    end
+
+    it 'should fail if there are any parameters' do
+      expect { subject.run(Object.new) }.to raise_error(ArgumentError)
+    end
+  end
 end
